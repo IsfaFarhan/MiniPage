8de4ac20339664abaa016be50dd7d04704ee7a3c
IsfaFarhan <157976265+IsfaFarhan@users.noreply.github.com>
2024-04-26 23:01:04 +0800

    Final - ihiks

--- interac.js
+++ interac.js
@@ -1,6 +1,13 @@
+function onDisplay() {
+    // Check if the screen width is at most 760px
+    if (window.innerWidth <= 767) {
+        let appCollection = document.getElementById('appsCollection');
+        appCollection.classList.toggle('appsCollectionActive');
+    }
+}
 
-function onDisplay(){
-    let appCollection = document.getElementById('appsCollection')
-    appCollection.classList.toggle('appsCollectionActive')
+function onDisplayHori(){
+    let appCollection = document.getElementById('appsCollectionHori')
+    appCollection.classList.toggle('appsCollectionHoriActive')
 
 }
